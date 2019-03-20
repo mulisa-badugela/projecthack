7f38d605b7aae70b762f97fb1aaae55075666a8b
Mulisa Badugela <Mrbadugela@gmail.com>
2019-03-20 02:05:49 +0200

    added tests for sorting algorithms

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,5 @@
 from projecthack import recursion
+from projecthack import sorting
 
 
 def test_sum_array():
@@ -24,3 +25,19 @@ def test_reverse():
 
     assert myFunction.reverse('cook') == 'kooc', 'incorrect'
     assert myFunction.reverse('hello') == 'olleh', 'incorrect'
+
+
+def test_bubble_sort():
+
+    assert myFunction.bubble_sort([2,3,1,4,6,5]) == [1,2,3,4,5,6], 'incorrect'
+    assert myFunction.bubble_sort([5,6,3,9,2,8,13,0]) == [0, 2, 3, 5, 6, 8, 9, 13], 'incorrect'
+
+def test_merge_sort():
+
+    assert myFunction.merge_sort([2,3,1,4,6,5]) == [1,2,3,4,5,6], 'incorrect'
+    assert myFunction.merge_sort([5,6,3,9,2,8,13,0]) == [0, 2, 3, 5, 6, 8, 9, 13], 'incorrect'
+
+def test_quick_sort():
+
+    assert myFunction.quick_sort([2,3,1,4,6,5]) == [1,2,3,4,5,6], 'incorrect'
+    assert myFunction.quick_sort([5,6,3,9,2,8,13,0]) == [0, 2, 3, 5, 6, 8, 9, 13], 'incorrect'
